5f33345e0daacea469f53b08df628e582fb236d6
Jeri Ramadhan <jeriramadhan25@gmail.com>
2020-08-10 15:31:19 +0700

    tambah data

--- application/models/Model_auth.php
+++ application/models/Model_auth.php
@@ -33,8 +33,8 @@ class Model_auth extends CI_Model
 			if($query->num_rows() == 1) {
 				$result = $query->row_array();
 
-				$hash_password = md5($password, $result['password']);
-				if($hash_password === true) {
+				// $hash_password = $password, $result['password'];
+				if(md5($password) == $result['password']) {
 					return $result;	
 				}
 				else {
